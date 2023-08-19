a3b498a89cb8e1588603cc724701f19bfc5d9115
egliette <leminhtu74@gmail.com>
2023-08-19 10:00:08 +0700

    add download image from url feature

---
+++ utils.py
@@ -0,0 +1,23 @@
+import requests
+import numpy as np
+from PIL import Image
+from io import BytesIO
+
+def download_image(url, save_path=None):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            img_data = BytesIO(response.content)
+            img = Image.open(img_data)
+            img_array = np.array(img)
+            
+            if save_path:
+                img.save(save_path)
+            
+            return img_array
+        else:
+            print(f"Failed to download image from {url}")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
