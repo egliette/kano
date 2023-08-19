600969f0212514d9140c232e6bef1ba97eab0cbc
egliette <leminhtu74@gmail.com>
2023-08-19 10:12:17 +0700

    add remove folder feature

---
+++ file_utils.py
@@ -0,0 +1,9 @@
+import shutil
+
+def remove_folder(folder_path):
+    try:
+        shutil.rmtree(folder_path)
+        print(f"Folder '{folder_path}' and its contents removed successfully.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
