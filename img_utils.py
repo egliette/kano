342d20acb38f6e9ad817362a4de2a2aab1eba0b7
egliette <leminhtu74@gmail.com>
2023-08-21 08:41:03 +0700

    add return np array and save image when drawing bbox feature

--- img_utils.py
+++ img_utils.py
@@ -1,7 +1,9 @@
+import io
+from io import BytesIO
+
 import requests
 import numpy as np
 from PIL import Image
-from io import BytesIO
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
@@ -36,7 +38,6 @@ def load_image_as_numpy(image_path, target_size=None):
         print(f"An error occurred: {e}")
         return None
 
-
 def plot_image(image, title=None):
     try:
         if isinstance(image, str):
@@ -53,7 +54,7 @@ def plot_image(image, title=None):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-def draw_bbox_and_show(image, top, right, bottom, left):
+def draw_bbox_and_show(image, top, right, bottom, left, save_path=None):
     try:
         if isinstance(image, str):
             image = Image.open(image)
@@ -75,5 +76,18 @@ def draw_bbox_and_show(image, top, right, bottom, left):
 
         plt.axis('off')
         plt.show()
+
+        if save_path:
+            plt.savefig(save_path, format='png')
+            output_image = Image.open(save_path)
+        else:
+            buffer = io.BytesIO()
+            plt.savefig(buffer, format='png')
+            buffer.seek(0)  
+            output_image = Image.open(buffer)
+
+        return np.array(output_image)
+    
     except Exception as e:
         print(f"An error occurred: {e}")
+        return None
