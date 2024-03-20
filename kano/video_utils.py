57361ce1f12aba95395dd34afc5720406578b4e6
unknown <leminhtu74@gmail.com>
2024-03-20 09:12:24 +0700

    feat: add video cutting feature

--- kano/video_utils.py
+++ kano/video_utils.py
@@ -1,10 +1,17 @@
 import os
+import uuid
 
 import cv2
+from moviepy.editor import VideoFileClip
 from pytube import YouTube
 from tqdm import tqdm
 
 from kano.file_utils import create_folder
+
+
+def generate_random_filename():
+    random_uuid = uuid.uuid4()
+    return str(random_uuid)
 
 
 def download_youtube_video(url, filename):
@@ -53,3 +60,32 @@ def extract_frames(video_path, target_folder, seconds_interval):
         cv2.imwrite(frame_filename, frame)
 
     cap.release()
+
+
+def cut_video(input_video_path, output_video_path, start_second, end_second):
+    cap = cv2.VideoCapture(input_video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    start_frame = int(start_second * fps)
+    end_frame = int(end_second * fps)
+
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    temp_file_path = generate_random_filename() + ".avi"
+    out = cv2.VideoWriter(temp_file_path, fourcc, fps, (width, height))
+
+    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+    while cap.isOpened() and cap.get(cv2.CAP_PROP_POS_FRAMES) <= end_frame:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        out.write(frame)
+
+    cap.release()
+    out.release()
+
+    clip = VideoFileClip(temp_file_path)
+    clip.write_videofile(output_video_path, codec="libx264", fps=clip.fps)
+
+    os.remove(temp_file_path)
