2646c793f5753def55739472faccecdddc3f5b7f
unknown <leminhtu74@gmail.com>
2024-03-24 22:13:17 +0700

    feat: add video concatenating function

--- kano/image_utils.py
+++ kano/image_utils.py
@@ -1,3 +1,4 @@
+import copy
 from io import BytesIO
 
 import cv2
@@ -100,3 +101,90 @@ def shift_image(image, dx, dy, new_height=None, new_width=None):
         (padded_image.shape[1], padded_image.shape[0]),
     )
     return shifted_image
+
+
+def pad_image(image, target_size):
+    # target_size = (height, width)
+    height, width = image.shape[:2]
+
+    pad_height = max(0, target_size[0] - height)
+    pad_width = max(0, target_size[1] - width)
+
+    top_pad = pad_height // 2
+    bottom_pad = pad_height - top_pad
+    left_pad = pad_width // 2
+    right_pad = pad_width - left_pad
+
+    padded_image = cv2.copyMakeBorder(
+        image.copy(),
+        top_pad,
+        bottom_pad,
+        left_pad,
+        right_pad,
+        cv2.BORDER_CONSTANT,
+        value=(0, 0, 0),
+    )
+
+    return padded_image
+
+
+def concatenate_images(image_list, padding_size=0):
+    # ensure image_list is a 2D list
+    new_image_list = copy.deepcopy(image_list)
+    if not isinstance(image_list[0], list):
+        new_image_list = [copy.deepcopy(image_list)]
+
+    rows = len(new_image_list)
+    cols = max(len(row) for row in new_image_list)
+
+    for image_row in new_image_list:
+        image_row += [None] * (cols - len(image_row))
+
+    # pad every images
+    max_height = max(
+        image.shape[0]
+        for row in new_image_list
+        for image in row
+        if image is not None
+    )
+    max_width = max(
+        image.shape[1]
+        for row in new_image_list
+        for image in row
+        if image is not None
+    )
+
+    padded_image_list = list()
+    for i, row in enumerate(new_image_list):
+        padded_image_list.append(list())
+        for j, image in enumerate(row):
+            padded_image = np.zeros((max_height, max_width, 3), dtype=np.uint8)
+            if image is not None:
+                padded_image = pad_image(image, (max_height, max_width))
+            padded_image_list[i].append(padded_image)
+
+    concatenated_image = np.zeros(
+        (
+            max_height * rows + padding_size * (rows - 1),
+            max_width * cols + padding_size * (cols - 1),
+            3,
+        ),
+        dtype=np.uint8,
+    )
+
+    for i, row in enumerate(padded_image_list):
+        for j, image in enumerate(row):
+
+            h, w, _ = image.shape
+            concatenated_image[
+                (i > 0) * padding_size
+                + i * max_height : (i > 0) * padding_size
+                + i * max_height
+                + h,
+                (j > 0) * padding_size
+                + j * max_width : (j > 0) * padding_size
+                + j * max_width
+                + w,
+            ] = image
+
+    return concatenated_image
--- kano/video_utils.py
+++ kano/video_utils.py
@@ -1,12 +1,15 @@
+import copy
 import os
 import uuid
 
 import cv2
+import numpy as np
 from moviepy.editor import VideoFileClip
 from pytube import YouTube
 from tqdm import tqdm
 
 from kano.file_utils import create_folder
+from kano.image_utils import concatenate_images
 
 
 def generate_random_filename():
@@ -88,4 +91,147 @@ def cut_video(input_video_path, output_video_path, start_second, end_second):
     clip = VideoFileClip(temp_file_path)
     clip.write_videofile(output_video_path, codec="libx264", fps=clip.fps)
 
+    os.remove(temp_file_path)
+
+
+def add_title(
+    image, title, font_scale=2, font_thickness=3, title_padding_size=10
+):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text_size = cv2.getTextSize(title, font, font_scale, font_thickness)[0]
+    text_x = (image.shape[1] - text_size[0]) // 2
+    text_y = image.shape[0] + title_padding_size + text_size[1]
+    height_diff = title_padding_size + text_size[1] + title_padding_size
+    padding = np.zeros((height_diff, image.shape[1], 3), dtype=np.uint8)
+    padded_image = np.vstack([image, padding])
+    cv2.putText(
+        padded_image,
+        title,
+        (text_x, text_y),
+        font,
+        font_scale,
+        (255, 255, 255),
+        font_thickness,
+    )
+    return padded_image
+
+
+def get_video_duration(video_path):
+    cap = cv2.VideoCapture(video_path)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    duration = frame_count / fps
+    cap.release()
+    return duration
+
+
+def concatenate_videos(
+    video_paths,
+    titles=None,
+    output_video_path="concatenated_video.mp4",
+    total_seconds=None,
+    font_scale=2,
+    font_thickness=3,
+    title_padding_size=10,
+    frame_padding_size=10,
+):
+    # ensure video_paths is a 2D list
+    new_video_paths = copy.deepcopy(video_paths)
+    if not isinstance(video_paths[0], list):
+        new_video_paths = [copy.deepcopy(video_paths)]
+
+    rows = len(new_video_paths)
+    cols = max(len(row) for row in new_video_paths)
+
+    for video_path in new_video_paths:
+        video_path += [None] * (cols - len(video_path))
+
+    # get target video duration
+    video_captures = list()
+    durations = list()
+
+    max_width = 0
+    max_height = 0
+    sample_cap = None
+    for i, row in enumerate(new_video_paths):
+        video_captures.append(list())
+        for video_path in row:
+            if video_path is not None:
+                sample_cap = cv2.VideoCapture(video_path)
+                width = int(sample_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                height = int(sample_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                max_width = max(width, max_width)
+                max_height = max(height, max_height)
+                video_captures[i].append(sample_cap)
+                durations.append(get_video_duration(video_path))
+            else:
+                video_captures[i].append(None)
+
+    min_duration = min(durations)
+    if total_seconds is not None:
+        min_duration = min(min_duration, total_seconds)
+
+    # init output video
+    fps = int(sample_cap.get(cv2.CAP_PROP_FPS))
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text_size = cv2.getTextSize("SAMPLE", font, font_scale, font_thickness)[0]
+
+    output_width = max_width * cols + frame_padding_size * (cols - 1)
+    output_height = (
+        max_height + (text_size[1] + title_padding_size * 2)
+    ) * rows + frame_padding_size * (rows - 1)
+
+    temp_file_path = generate_random_filename() + ".avi"
+    output_video = cv2.VideoWriter(
+        temp_file_path, fourcc, fps, (output_width, output_height)
+    )
+    num_frames = int(min_duration * fps)
+
+    for __ in tqdm.tqdm(range(num_frames), desc="Videos concatenating"):
+        frames = list()
+        for row in range(rows):
+            frames.append(list())
+            for col in range(cols):
+                cap = video_captures[row][col]
+                if (
+                    len(titles) < row + 1
+                    or len(titles[row]) < col + 1
+                    or titles[row][col] is None
+                ):
+                    title = " "
+                else:
+                    title = titles[row][col]
+
+                frame = np.zeros((max_height, max_width, 3), dtype=np.uint8)
+                if cap is not None:
+                    __, frame = cap.read()
+
+                frame = add_title(
+                    frame,
+                    title,
+                    font_scale,
+                    font_thickness,
+                    title_padding_size,
+                )
+
+                frames[row].append(frame)
+
+        concatenated_frame = concatenate_images(frames, frame_padding_size)
+
+        output_video.write(concatenated_frame)
+
+    for row_caps in video_captures:
+        for cap in row_caps:
+            if cap is not None:
+                cap.release()
+
+    output_video.release()
+
+    print("Preparing output video...")
+    clip = VideoFileClip(temp_file_path)
+    clip.write_videofile(
+        output_video_path, codec="libx264", fps=clip.fps, logger=None
+    )
     os.remove(temp_file_path)
